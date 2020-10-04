94f02eeb513a733a0826075c73b05df291282ef8
tiltproofRain <srijannr1@gmail.com>
2020-10-04 16:35:49 +0530

    added jest tests

--- ave/tests/lexTests.ts
+++ ave/tests/lexTests.ts
@@ -2,10 +2,9 @@ import Lexer from '../lexer/lexer';
 import { getDirectories, getFiles } from './helpers';
 import debug = require('../debug/debug');
 import fs = require('fs');
-import util = require('util');
 import chalk = require('chalk');
 
-const lexTestDir: string = './test';
+const lexTestDir: string = './test/lexer';
 
 function runLexer(filePath: string) {
   console.log('running lexer on ' + chalk.yellow(filePath), '\n');
@@ -18,14 +17,12 @@ function runLexer(filePath: string) {
 export default function runLexerTests() {
   // folders containing the test files
   let count = 0;
-  const folderNames: string[] = getDirectories(lexTestDir);
-  for (const f of folderNames) {
-    const testFileNames: string[] = getFiles(lexTestDir + '/' + f);
-    for (const fileName of testFileNames) {
-      const path: string = lexTestDir + '/' + f + '/' + fileName;
-      count++;
-      runLexer(path);
-    }
+
+  const testFileNames: string[] = getFiles(lexTestDir);
+  for (const fileName of testFileNames) {
+    const path: string = lexTestDir + '/' + fileName;
+    count++;
+    runLexer(path);
   }
 
   console.log(chalk.black.bgGreenBright('LEXER TESTS COMPLETE'), `: ${count} files lexed.`);
